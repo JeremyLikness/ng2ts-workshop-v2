af60cfe8c66a6f3d8a46f5eb33f4e5d598a18ce8
Jeremy Likness <jeremy@jeremylikness.com>
2017-02-11 16:23:11 -0500

    Focus with timeout

--- 20-CompDirPipe/devnexus-ts-ng2/src/app/focus.directive.ts
+++ 20-CompDirPipe/devnexus-ts-ng2/src/app/focus.directive.ts
@@ -1,12 +1,25 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appFocus]'
 })
 export class FocusDirective {
 
+  private _timeout: number = 0;
+
+  @Input('appFocusTimeout')
+  public set timeout(val: number) {
+    this._timeout = val;
+    setTimeout(() => {
+      this.elem.nativeElement.focus();
+    }, this._timeout);
+  }
+
+  public get timeout(): number {
+    return this._timeout;
+  }
+
   constructor(private elem: ElementRef) {
-    setTimeout(() => this.elem.nativeElement.focus(), 0);
   }
 
 }
